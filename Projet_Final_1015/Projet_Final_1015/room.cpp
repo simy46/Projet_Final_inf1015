26a3d34b7b5c582473a503ec1f578b62c373cb82
Missipsa Annane <missiannane@yahoo.com>
2023-11-27 10:16:17 -0500

    quick

--- Projet_Final_1015/Projet_Final_1015/room.cpp
+++ Projet_Final_1015/Projet_Final_1015/room.cpp
@@ -5,34 +5,15 @@ Room::Room() : line_(-1), column_(-1), floor_(-1), size_(-1) {}
 
 Room::Room(int l, int c, int e, int s) : line_(l), column_(c), floor_(e), size_(s) {}
 
-int Room::getLine() const {
-	return line_;
-}
 
-int Room::getColumn() const {
-	return column_;
-}
+//######## GETTERS ########//
+int Room::getLine() const { return line_; }
+int Room::getColumn() const { return column_; }
+int Room::getSize() const { return size_; }
+int Room::getFloor() const { return floor_; }
 
-int Room::getSize() const {
-	return size_;
-}
-
-int Room::getFloor() const {
-	return floor_;
-}
-
-void Room::setLine(int l) {
-	line_ = l;
-}
-
-void Room::setColumn(int c) {
-	column_ = c;
-}
-
-void Room::setSize(int s) {
-	size_ = s;
-}
-
-void Room::setFloor(int f) {
-	floor_ = f;
-}
+//######## SETTERS ########//
+void Room::setLine(int l) { line_ = l; }
+void Room::setColumn(int c) { column_ = c; }
+void Room::setSize(int s) { size_ = s; }
+void Room::setFloor(int f) { floor_ = f; }
